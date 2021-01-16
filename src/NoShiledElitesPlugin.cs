dcc7c829c268aa87754974b2bfc32d0de3adb6a1
adnan <adnan97m@gmail.com>
2021-01-16 10:45:32 +0100

    remove full tierdef overwrite

--- src/NoShiledElitesPlugin.cs
+++ src/NoShiledElitesPlugin.cs
@@ -2,7 +2,6 @@
 using BepInEx;
 using R2API.Utils;
 using RoR2;
-using UnityEngine;
 
 namespace NoShieldElitesMod
 {
@@ -13,85 +12,24 @@ namespace NoShieldElitesMod
     {
         public void Awake()
         {
-            On.RoR2.CombatDirector.PrepareNewMonsterWave += CombatDirectorOnPrepareNewMonsterWave;
+            On.RoR2.CombatDirector.PrepareNewMonsterWave += OnPrepareNewMonsterWave;
         }
 
-        private static void CombatDirectorOnPrepareNewMonsterWave(On.RoR2.CombatDirector.orig_PrepareNewMonsterWave orig, CombatDirector self, DirectorCard monsterCard)
+        private static void OnPrepareNewMonsterWave(On.RoR2.CombatDirector.orig_PrepareNewMonsterWave orig, CombatDirector self, DirectorCard monsterCard)
         {
-            //float costMultiplier = self.GetFieldValue<float>("baseEliteCostMultiplier");
-            //float damageBoostCoefficient = self.GetFieldValue<float>("baseEliteDamageBoostCoefficient");
-            //float healthBoostCoefficient = self.GetFieldValue<float>("baseEliteHealthBoostCoefficient");
+            CombatDirector.EliteTierDef[] eliteTiers = self.GetFieldValue<CombatDirector.EliteTierDef[]>("eliteTiers");
 
-            float costMultiplier = 6f;
-            float damageBoostCoefficient = 2f;
-            float healthBoostCoefficient = 4f;
-
-            CombatDirector.EliteTierDef[] eliteTiers = {
-                new CombatDirector.EliteTierDef
-                {
-                    costMultiplier = 1f,
-                    damageBoostCoefficient = 1f,
-                    healthBoostCoefficient = 1f,
-                    eliteTypes = new EliteIndex[]
-                    {
-                        EliteIndex.None
-                    },
-                    isAvailable = new Func<bool>(NotEliteOnlyArtifactActive)
-                },
-                new CombatDirector.EliteTierDef
-                {
-                    costMultiplier = costMultiplier,
-                    damageBoostCoefficient = damageBoostCoefficient,
-                    healthBoostCoefficient = healthBoostCoefficient,
-                    eliteTypes = new EliteIndex[]
-                    {
-                        EliteIndex.Fire,
-                        EliteIndex.Lightning,
-                        EliteIndex.Ice
-                    },
-                    isAvailable = new Func<bool>(NotEliteOnlyArtifactActive)
-                },
-                new CombatDirector.EliteTierDef
-                {
-                    costMultiplier = Mathf.LerpUnclamped(1f, costMultiplier, 0.5f),
-                    damageBoostCoefficient = Mathf.LerpUnclamped(1f, damageBoostCoefficient, 0.5f),
-                    healthBoostCoefficient = Mathf.LerpUnclamped(1f, healthBoostCoefficient, 0.5f),
-                    eliteTypes = new EliteIndex[]
-                    {
-                        EliteIndex.Fire,
-                        EliteIndex.Lightning,
-                        EliteIndex.Ice
-                    },
-                    isAvailable = new Func<bool>(EliteOnlyArtifactActive)
-                },
-                new CombatDirector.EliteTierDef
-                {
-                    costMultiplier = costMultiplier * 6f,
-                    damageBoostCoefficient = damageBoostCoefficient * 3f,
-                    healthBoostCoefficient = healthBoostCoefficient * 4.5f,
-                    eliteTypes = new EliteIndex[]
-                    {
-                        EliteIndex.Poison,
-                        EliteIndex.Haunted
-                    },
-                    isAvailable = (() => Run.instance.loopClearCount > 0)
-                }
-            };
+            foreach (CombatDirector.EliteTierDef tier in eliteTiers)
+            {
+                tier.SetFieldValue(
+                    "eliteTypes",
+                    Array.FindAll(tier.eliteTypes, (EliteIndex index) => index != EliteIndex.Lightning)
+                );
+            }
 
             self.SetFieldValue<CombatDirector.EliteTierDef[]>("eliteTiers", eliteTiers);
 
             orig(self, monsterCard);
         }
-
-        private static bool EliteOnlyArtifactActive()
-        {
-            return RunArtifactManager.instance.IsArtifactEnabled(RoR2Content.Artifacts.eliteOnlyArtifactDef);
-        }
-
-        private static bool NotEliteOnlyArtifactActive()
-        {
-            return false == RunArtifactManager.instance.IsArtifactEnabled(RoR2Content.Artifacts.eliteOnlyArtifactDef);
-        }
     }
 }
-
